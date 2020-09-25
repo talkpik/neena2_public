f448fab745ac92a74d4d2629de206285da39d3f6
talkpik <talkpik2020@gmail.com>
2020-09-25 01:08:02 -0700

    Updates

---
+++ precache-manifest.86c39ba020089d7a5f59cc41a8df230f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09aae71a4c152d170bdf9fb151c5fb5e",
+    "url": "/neena2_public/index.html"
+  },
+  {
+    "revision": "6bc9dace6a331f365367",
+    "url": "/neena2_public/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "7a71e15fdcb0c71d6dc1",
+    "url": "/neena2_public/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6bc9dace6a331f365367",
+    "url": "/neena2_public/static/js/2.2d1be1c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neena2_public/static/js/2.2d1be1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a71e15fdcb0c71d6dc1",
+    "url": "/neena2_public/static/js/main.1baec044.chunk.js"
+  },
+  {
+    "revision": "1876e0e932eb3bada195",
+    "url": "/neena2_public/static/js/runtime-main.e86bfe08.js"
+  }
+]);
